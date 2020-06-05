5558dc6b1e970eec56a8af29a6b8a361657d61a8
Fullstack-Dev <fulldev@Fullstack-Devs-iMac-Pro.local>
2020-06-05 21:03:33 +1000

    fix

---
+++ precache-manifest.b59c99c71667c6faba27a067dd903c6b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9be8a7941e02ca1529a0f06df93eec4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc4adf254e44c4a8ba40",
+    "url": "./static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d24e8bbdd3bdcd6dc20a",
+    "url": "./static/css/main.ede39b44.chunk.css"
+  },
+  {
+    "revision": "bc4adf254e44c4a8ba40",
+    "url": "./static/js/2.946036ad.chunk.js"
+  },
+  {
+    "revision": "bee297756a06e64fa2fb17ce989a9af6",
+    "url": "./static/js/2.946036ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24e8bbdd3bdcd6dc20a",
+    "url": "./static/js/main.39c6e0a7.chunk.js"
+  },
+  {
+    "revision": "bdc8c14741a5987235ce",
+    "url": "./static/js/runtime-main.03fab981.js"
+  },
+  {
+    "revision": "d5e530f5bae0d5f73b893c5adccb44ea",
+    "url": "./static/media/index_icon2.d5e530f5.png"
+  },
+  {
+    "revision": "4c813c8c0539a1144ae5212476ee39dc",
+    "url": "./static/media/index_icon3.4c813c8c.png"
+  },
+  {
+    "revision": "4c5d01da57e3cc57f8c90fbf56c7a494",
+    "url": "./static/media/logo.4c5d01da.png"
+  }
+]);
